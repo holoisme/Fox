08e266975dbfb6d99e1003dbb5002fcca5f2fe84
HoloInJava <yousseflaraki99@gmail.com>
2023-05-21 21:27:27 +0200

    added double handling (+ number types)

--- src/holo/interpreter/values/interfaces/INumber.java
+++ src/holo/interpreter/values/interfaces/INumber.java
@@ -4,7 +4,17 @@ public interface INumber {
 	
 	public int getInteger();
 	public float getFloat();
+	public double getDouble();
 	
-	public default boolean isInteger() { return getInteger() == getFloat(); }
+	public boolean isInteger();
+	
+	public default boolean isFloat() { return numberType() == NumberType.FLOAT; }
+	public default boolean isDouble() { return numberType() == NumberType.DOUBLE; }
+	
+	public NumberType numberType();
+	
+	public static enum NumberType {
+		INTEGER, FLOAT, DOUBLE, BYTE, SHORT, LONG, CHAR
+	}
 	
 }
