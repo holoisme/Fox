858376140df428f4c734da465b33018021c507aa
HoloInJava <yousseflaraki99@gmail.com>
2023-05-21 21:17:29 +0200

    added double and char cast type

--- src/holo/interpreter/types/CastingType.java
+++ src/holo/interpreter/types/CastingType.java
@@ -2,7 +2,7 @@ package holo.interpreter.types;
 
 public enum CastingType {
 	
-	INTEGER("int"), FLOAT("float"), STRING("string"), BOOLEAN("boolean");
+	INTEGER("int"), FLOAT("float"), DOUBLE("double"), CHARACTER("char"), STRING("string"), BOOLEAN("boolean");
 	
 	private String symbol;
 	
@@ -15,8 +15,10 @@ public enum CastingType {
 	public static CastingType get(String type) {
 		if(type.equals("int")) return INTEGER;
 		else if(type.equals("float")) return FLOAT;
+		else if(type.equals("double")) return DOUBLE;
 		else if(type.equals("string")) return STRING;
 		else if(type.equals("boolean")) return BOOLEAN;
+		else if(type.equals("char")) return CHARACTER;
 		
 		return null;
 	}
