ca889316d7e0ccede8592274961164321c42f7fc
HoloInJava <yousseflaraki99@gmail.com>
2023-05-21 21:18:41 +0200

    added static fields and static method calls

--- src/holo/interpreter/values/portal/ClassInstanciator.java
+++ src/holo/interpreter/values/portal/ClassInstanciator.java
@@ -1,21 +1,36 @@
 package holo.interpreter.values.portal;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Map;
 
 import holo.errors.RuntimeError;
+import holo.errors.portal.NoSuchMethodError;
 import holo.interpreter.Interpreter;
 import holo.interpreter.RuntimeResult;
 import holo.interpreter.values.Value;
+import holo.interpreter.values.interfaces.ICallHandler;
 import holo.interpreter.values.interfaces.IInstanciable;
 
-public class ClassInstanciator implements Value, IInstanciable {
+public class ClassInstanciator implements Value, IInstanciable, ICallHandler {
 	
 	private Class<?> cl;
 	
+	private Map<String, Field> staticFields;
+	
 	public ClassInstanciator(Class<?> cl) {
 		this.cl = cl;
+		
+//		Field[] declaredFields = cl.getDeclaredFields();
+//		staticFields = new HashMap<>();
+//		for (Field field : declaredFields)
+//		    if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+//		    	staticFields.put(field.getName(), field);
+//		    	
+//		    }
 	}
 
 	@Override
@@ -23,23 +38,15 @@ public class ClassInstanciator implements Value, IInstanciable {
 		Object[] convertedArgs = new Object[args.length];
 		Class<?>[] argsClasses = new Class<?>[args.length];
 		
-		System.out.println("-------");
 		for(int i = 0; i < args.length; i++) {
 			convertedArgs[i] = args[i].toJavaObject();
 			argsClasses[i] = args[i].toJavaClass(convertedArgs[i]);
-			System.out.println(argsClasses[i].getSimpleName());
 		}
-		System.out.println("-------");
 		
-		System.out.println("Instanciate " + cl.getSimpleName());
 		try {
 			Constructor<?> constructor = cl.getConstructor(argsClasses);
-			System.out.println("Args: " + Arrays.toString(convertedArgs));
-			System.out.println(constructor);
 			
 			Object instance = constructor.newInstance(convertedArgs);
-			System.out.println("letsgo !");
-			System.out.println("\n");
 			return onGoing.buffer(Value.convertJavaToFoxValue(instance));
 		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			return onGoing.failure(new RuntimeError(e.getMessage(), null));
@@ -48,6 +55,11 @@ public class ClassInstanciator implements Value, IInstanciable {
 
 	@Override
 	public String typeName() {
+		return cl.getSimpleName() + " class";
+	}
+	
+	@Override
+	public String toString() {
 		return cl.getSimpleName() + " class";
 	}
 
@@ -62,10 +74,24 @@ public class ClassInstanciator implements Value, IInstanciable {
 	}
 
 	public Value pointGet(String key) {
+		try {
+			Field field = cl.getDeclaredField(key);
+			if(!Modifier.isStatic(field.getModifiers()) || field.isAnnotationPresent(FoxIgnore.class))
+				return null;
+			return Value.convertJavaToFoxValue(field.get(null));
+		} catch (Exception e) { e.printStackTrace(); }
 		return null;
 	}
 
-	public Value pointSet(String key, Value value) {
+	public Value pointSet(String key, Value v) {
+		try {
+			Field field = cl.getDeclaredField(key);
+			if(field == null || !Modifier.isStatic(field.getModifiers()) || field.isAnnotationPresent(FoxIgnore.class))
+				return null;
+			field.set(null, v.toJavaObject());
+			return v;
+		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) { e.printStackTrace(); }
+		
 		return null;
 	}
 
@@ -75,6 +101,27 @@ public class ClassInstanciator implements Value, IInstanciable {
 
 	public Value arraySet(Value key, Value value) {
 		return null;
+	}
+
+	@Override
+	public RuntimeResult callMethod(RuntimeResult onGoing, String name, Value... args) {
+		Object[] convertedArgs = new Object[args.length];
+		Class<?>[] argsClasses = new Class<?>[args.length];
+		
+		for(int i = 0; i < args.length; i++) {
+			convertedArgs[i] = args[i].toJavaObject();
+			argsClasses[i] = args[i].toJavaClass(convertedArgs[i]);
+		}
+		
+		try {
+			Method method = cl.getMethod(name, argsClasses);
+			if(method == null || method.isAnnotationPresent(FoxIgnore.class))
+				return onGoing.failure(new NoSuchMethodError(name, argsClasses, typeName(), null));
+			
+			Object returnedObject = method.invoke(null, convertedArgs);
+			
+			return onGoing.success(Value.convertJavaToFoxValue(returnedObject));
+		} catch (Exception e) { return onGoing.failure(new RuntimeError(e.getMessage(), null)); }
 	}
 	
 }
