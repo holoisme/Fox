5a40aab6975be92cea4e5b10d2e349f04adbaa80
HoloInJava <yousseflaraki99@gmail.com>
2023-05-21 21:32:46 +0200

    string's 'length' uses optimized integer values and 'charAt' uses char value

--- src/holo/interpreter/values/prototypes/StringPrototype.java
+++ src/holo/interpreter/values/prototypes/StringPrototype.java
@@ -2,6 +2,7 @@ package holo.interpreter.values.prototypes;
 
 import holo.errors.WrongTypeError;
 import holo.interpreter.values.interfaces.INumber;
+import holo.interpreter.values.primitives.CharValue;
 import holo.interpreter.values.primitives.IntegerValue;
 import holo.interpreter.values.primitives.StringValue;
 
@@ -13,12 +14,12 @@ public class StringPrototype  {
 		PROTOTYPE = new Prototype<>("String");
 		
 		PROTOTYPE.addFunction("length", (self, runtime, args) -> {
-			return runtime.buffer(new IntegerValue(self.length()));
+			return runtime.buffer(IntegerValue.get(self.length()));
 		});
 		
 		PROTOTYPE.addFunction("charAt", (self, runtime, args) -> {
 			if(args[0] instanceof INumber num)
-				return runtime.buffer(new StringValue(""+self.getValue().charAt(num.getInteger())));
+				return runtime.buffer(new CharValue(self.getValue().charAt(num.getInteger())));
 			return runtime.failure(new WrongTypeError("number", args[0].toString(), null));
 		}, "index");
 		
