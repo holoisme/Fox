d17f78ad30508bad4de1f89c94407f2c76ab288d
HoloInJava <yousseflaraki99@gmail.com>
2023-06-23 21:02:31 +0200

    taking sequence while creating a new instance (better error tracking)

--- src/holo/interpreter/values/interfaces/IInstanciable.java
+++ src/holo/interpreter/values/interfaces/IInstanciable.java
@@ -3,9 +3,10 @@ package holo.interpreter.values.interfaces;
 import holo.interpreter.Interpreter;
 import holo.interpreter.RuntimeResult;
 import holo.interpreter.values.Value;
+import holo.lang.lexer.Sequence;
 
 public interface IInstanciable {
 	
-	public RuntimeResult createInstance(Interpreter interpreter, RuntimeResult onGoing, Value... args);
+	public RuntimeResult createInstance(Interpreter interpreter, RuntimeResult onGoing, Sequence sequence, Value... args);
 	
 }
