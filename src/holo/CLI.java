84a48d3adde5756632ac0b40b9aed84088bb4f77
HoloInJava <yousseflaraki99@gmail.com>
2023-06-23 20:57:01 +0200

    updated root directory

--- src/holo/CLI.java
+++ src/holo/CLI.java
@@ -7,16 +7,11 @@ import holo.interpreter.Interpreter;
 public class CLI {
 	
 	public static void main(String[] args) {
-		// Interpreter interpreter = new Interpreter(new File("./math-evaluator").toPath());
+		Interpreter interpreter = new Interpreter(new File("./").toPath());
 		
-		// interpreter.setDebug(false);
-		// interpreter.setPerformance(false);
-		
-		// interpreter.execute("Sandbox.fox");
-		
-		Interpreter interpreter = new Interpreter(new File(args[0]).getParentFile().toPath());
 		interpreter.setDebug(false);
 		interpreter.setPerformance(false);
+		
 		interpreter.execute(args[0]);
 	}
 	
