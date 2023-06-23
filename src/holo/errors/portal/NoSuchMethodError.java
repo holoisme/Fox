a5c5a48d8975de47198e786db2c89dc7e8717b70
HoloInJava <yousseflaraki99@gmail.com>
2023-06-23 21:06:17 +0200

    better display

--- src/holo/errors/portal/NoSuchMethodError.java
+++ src/holo/errors/portal/NoSuchMethodError.java
@@ -1,14 +1,13 @@
 package holo.errors.portal;
 
-import java.util.Arrays;
-
 import holo.errors.RuntimeError;
+import holo.interpreter.nodes.ReflectionUtils;
 import holo.lang.lexer.Sequence;
 
 public class NoSuchMethodError extends RuntimeError {
 
 	public NoSuchMethodError(String name, Class<?>[] classes, String typeName, Sequence sequence) {
-		super("No such method", "Cannot find " + name + "("+Arrays.toString(classes)+") method in " + typeName, sequence);
+		super("No such method", "Cannot find " + name + "("+ReflectionUtils.toString(classes)+") method in " + typeName, sequence);
 	}
 
 }
