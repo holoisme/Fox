69b6fe61b2a0b8e49e0bc33765589d48325696ef
HoloInJava <yousseflaraki99@gmail.com>
2023-06-27 09:43:38 +0200

    pre-instance value node for performance

---
+++ src/holo/interpreter/nodes/values/ValueNode.java
@@ -0,0 +1,19 @@
+package holo.interpreter.nodes.values;
+
+import holo.interpreter.Interpreter;
+import holo.interpreter.contexts.Context;
+import holo.interpreter.nodes.Node;
+import holo.interpreter.values.Value;
+import holo.lang.lexer.Sequence;
+
+public record ValueNode(Value value, Sequence sequence) implements Node {
+	
+	public String toString() {
+		return value + "";
+	}
+	
+	public Value interpret(Context parentContext, Interpreter interpreter) {
+		return value;
+	}
+	
+}
