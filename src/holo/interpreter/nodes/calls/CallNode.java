5f1057e78c79ff1e06e4bc685bb93a717007da23
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 19:02:21 +0200

    call in CallHandler results in the latter to handle it (+ undefined support)

--- src/holo/interpreter/nodes/calls/CallNode.java
+++ src/holo/interpreter/nodes/calls/CallNode.java
@@ -1,6 +1,5 @@
 package holo.interpreter.nodes.calls;
 
-import holo.errors.CannotAccessError;
 import holo.errors.CannotCallError;
 import holo.errors.IncorrectNumberOfArguments;
 import holo.interpreter.Interpreter;
@@ -10,12 +9,13 @@ import holo.interpreter.nodes.ReflectionUtils;
 import holo.interpreter.nodes.helpers.args.NamedNode;
 import holo.interpreter.nodes.helpers.args.NamedValue;
 import holo.interpreter.nodes.var.OptionalChainingNode;
+import holo.interpreter.nodes.var.VarAccessNode;
 import holo.interpreter.nodes.var.VarPointAccessNode;
+import holo.interpreter.transcendental.TError;
 import holo.interpreter.values.Value;
 import holo.interpreter.values.interfaces.ICallHandler;
 import holo.interpreter.values.interfaces.ICallable;
 import holo.lang.lexer.Sequence;
-import holo.transcendental.TError;
 
 public record CallNode(Node access, Node[] args, NamedNode[] optionalArguments, Sequence sequence) implements Node {
 	
@@ -27,48 +27,55 @@ public record CallNode(Node access, Node[] args, NamedNode[] optionalArguments, 
 		Value hostValue = null;
 		Value accessValue = null;
 		
-		Value[] argsValues = new Value[args.length];
+		final Value[] argsValues = new Value[args.length];
 		for(int i = 0; i < args.length; i++)
 			argsValues[i] = args[i].interpret(parentContext, interpreter);
 		
-		NamedValue[] optionalValues = new NamedValue[optionalArguments.length];
+		final NamedValue[] optionalValues = new NamedValue[optionalArguments.length];
 		for(int i = 0; i < optionalArguments.length; i++)
 			optionalValues[i] = new NamedValue(optionalArguments[i].name(), optionalArguments[i].node().interpret(parentContext, interpreter));
 		
-		if(access instanceof VarPointAccessNode pointAccess) {
+		if(access instanceof VarAccessNode var) {
+			final String varName = var.varName();
+			Context container = parentContext.getFirstParentThatHasKey(varName);
+			
+			if(container instanceof ICallHandler wrapper)
+				return wrapper.callMethod(varName, argsValues, optionalValues, interpreter, sequence);
+			accessValue = container.get(varName);
+		} else if(access instanceof VarPointAccessNode pointAccess) {
 			hostValue = pointAccess.access().interpret(parentContext, interpreter);
 			
 			if(hostValue instanceof ICallHandler wrapper)
-				return handleFoxPortalWrapper(wrapper, pointAccess.varName(), argsValues, parentContext, interpreter);
+				return wrapper.callMethod(pointAccess.varName(), argsValues, optionalValues, interpreter, sequence);
 			
-			accessValue = hostValue.pointGet(pointAccess.varName());
-			if(accessValue == null)
-				throw new TError(new CannotAccessError(hostValue.typeName(), access.sequence()));
+			accessValue = hostValue.pointGet(pointAccess.varName(), sequence);
 		} else if(access instanceof OptionalChainingNode pointAccess) {
 			hostValue = pointAccess.access().interpret(parentContext, interpreter);
 			
-			if(hostValue == Value.NULL)
-				return Value.NULL;
+			if(hostValue.nullOrUndefined())
+				return Value.UNDEFINED;
 			
 			if(hostValue instanceof ICallHandler wrapper)
-				return handleFoxPortalWrapper(wrapper, pointAccess.varName(), argsValues, parentContext, interpreter);
+				return wrapper.callMethod(pointAccess.varName(), argsValues, optionalValues, interpreter, sequence);
 			
-			accessValue = hostValue.pointGet(pointAccess.varName());
-			if(accessValue == null)
-				throw new TError(new CannotAccessError(hostValue.typeName(), access.sequence()));
+			accessValue = hostValue.pointGet(pointAccess.varName(), sequence);
 		} else accessValue = access.interpret(parentContext, interpreter);
 		
 		if(accessValue instanceof ICallable callable) {
-			int argNumber = callable.numberOfArguments();
+			final int argNumber = callable.numberOfArguments();
 			if(argNumber != -1 && argNumber != argsValues.length)
 				throw new TError(new IncorrectNumberOfArguments(argNumber, argsValues.length, accessValue.typeName(), sequence));
 			
-			return callable.call(hostValue, parentContext, interpreter, argsValues, optionalValues);
+			try {
+				return callable.call(hostValue, parentContext, interpreter, sequence, argsValues, optionalValues);
+			} catch(TError error) {
+				throw error.addToTrace(access.toString(), sequence);
+			}
 		} else throw new TError(new CannotCallError(accessValue.typeName(), access.sequence()));
 	}
 	
-	private Value handleFoxPortalWrapper(ICallHandler wrapper, String methodName, Value[] args, Context parentContext, Interpreter interpreter) {
-		return wrapper.callMethod(methodName, args);
-	}
+//	private Value handleFoxPortalWrapper(ICallHandler wrapper, String methodName, Value[] args, Interpreter interpreter, Sequence sequence) {
+//		return wrapper.callMethod(methodName, args, interpreter, sequence);
+//	}
 	
 }
