d12cc22f67f28e61b19c8a8867f988b19c4680ef
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 18:56:14 +0200

    added sequence argument for error handling

--- src/holo/interpreter/values/interfaces/ICallable.java
+++ src/holo/interpreter/values/interfaces/ICallable.java
@@ -4,10 +4,11 @@ import holo.interpreter.Interpreter;
 import holo.interpreter.contexts.Context;
 import holo.interpreter.nodes.helpers.args.NamedValue;
 import holo.interpreter.values.Value;
+import holo.lang.lexer.Sequence;
 
 public interface ICallable {
 	
-	public Value call(Value host, Context parentContext, Interpreter interpreter, Value[] args, NamedValue[] optionalArguments);
+	public Value call(Value host, Context parentContext, Interpreter interpreter, Sequence sequence, Value[] args, NamedValue[] optionalArguments);
 	public int numberOfArguments();
 	
 }
