c5cfa7a0e8c68edcc04471b1a64fb070bb4794e3
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 20:51:25 +0200

    precised that the iteration context is not enclosed

--- src/holo/interpreter/contexts/IterationContext.java
+++ src/holo/interpreter/contexts/IterationContext.java
@@ -3,7 +3,7 @@ package holo.interpreter.contexts;
 public class IterationContext extends SimpleContext {
 
 	public IterationContext(Context TRUE_PARENT) {
-		super("iteration", TRUE_PARENT);
+		super("iteration", TRUE_PARENT, false);
 	}
 	
 	public void clear() {
