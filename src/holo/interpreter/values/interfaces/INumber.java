c5c2351fae35ac7ccf550b3fbb6db6f0cd4ec6b3
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 19:23:14 +0200

    added support for all the Java number primitives (+correct operation type priority)

--- src/holo/interpreter/values/interfaces/INumber.java
+++ src/holo/interpreter/values/interfaces/INumber.java
@@ -1,20 +1,35 @@
 package holo.interpreter.values.interfaces;
 
+import holo.interpreter.Interpreter;
+import holo.interpreter.types.BinaryOperationType;
+import holo.interpreter.values.Value;
+import holo.interpreter.values.primitives.StringValue;
+import holo.lang.lexer.Sequence;
+
 public interface INumber {
 	
-	public int getInteger();
-	public float getFloat();
-	public double getDouble();
+	public static final StringValue T_NUMBER = new StringValue("number");
 	
-	public boolean isInteger();
-	
-	public default boolean isFloat() { return numberType() == NumberType.FLOAT; }
-	public default boolean isDouble() { return numberType() == NumberType.DOUBLE; }
+	int getInteger();
+	Number get();
+	boolean isInteger();
 	
 	public NumberType numberType();
 	
+	public Value handle(INumber left, BinaryOperationType operation, Interpreter interpreter, Sequence sequence);
+	
 	public static enum NumberType {
-		INTEGER, FLOAT, DOUBLE, BYTE, SHORT, LONG, CHAR
+		BYTE(true, true), CHAR(true, true), SHORT(true, true), INTEGER(true, true), LONG(true, false),
+		FLOAT(false, false), DOUBLE(false, false);
+		
+		private boolean integer, operationConvertToInteger;
+		private NumberType(boolean integer, boolean operationConvertToInteger) {
+			this.integer = integer;
+			this.operationConvertToInteger = operationConvertToInteger;
+		}
+		
+		public boolean isInteger() { return integer; }
+		public boolean doesOperationConvertToInteger() { return operationConvertToInteger; }
 	}
 	
 }
