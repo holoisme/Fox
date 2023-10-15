f7efcb52efb568c50c10870a73cfe942f93d42e1
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 20:00:21 +0200

    added sequence argument for error handling

--- src/holo/interpreter/LambdaExecutor.java
+++ src/holo/interpreter/LambdaExecutor.java
@@ -2,9 +2,10 @@ package holo.interpreter;
 
 import holo.interpreter.contexts.Context;
 import holo.interpreter.values.Value;
+import holo.lang.lexer.Sequence;
 
 public interface LambdaExecutor {
 	
-	public Value call(Value host, Context context, Interpreter interpreter, Value... args);
+	public Value call(Value host, Context context, Interpreter interpreter, Sequence sequence, Value... args);
 	
 }
