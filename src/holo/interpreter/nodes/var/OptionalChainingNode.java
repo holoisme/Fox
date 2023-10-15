0d45f701be2bc82b7533726e74225104f1e5cca3
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 20:17:07 +0200

    checks if access value is either null or undefined

--- src/holo/interpreter/nodes/var/OptionalChainingNode.java
+++ src/holo/interpreter/nodes/var/OptionalChainingNode.java
@@ -4,9 +4,9 @@ import holo.errors.CannotAccessError;
 import holo.interpreter.Interpreter;
 import holo.interpreter.contexts.Context;
 import holo.interpreter.nodes.Node;
+import holo.interpreter.transcendental.TError;
 import holo.interpreter.values.Value;
 import holo.lang.lexer.Sequence;
-import holo.transcendental.TError;
 
 public record OptionalChainingNode(Node access, String varName, Sequence sequence) implements Node {
 	
@@ -15,12 +15,12 @@ public record OptionalChainingNode(Node access, String varName, Sequence sequenc
 	}
 	
 	public Value interpret(Context parentContext, Interpreter interpreter) {
-		Value hostValue = access.interpret(parentContext, interpreter);
+		final Value hostValue = access.interpret(parentContext, interpreter);
 		
-		if(hostValue == Value.NULL)
-			return Value.NULL;
+		if(hostValue.nullOrUndefined())
+			return Value.UNDEFINED;
 		
-		Value pointGetValue = hostValue.pointGet(varName);
+		final Value pointGetValue = hostValue.pointGet(varName, sequence);
 		if(pointGetValue == null)
 			throw new TError(new CannotAccessError(hostValue.typeName(), access.sequence()));
 		
