3913f771f0ef237e01221b59dcf028de94f70814
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 20:15:26 +0200

    updating TError package path

--- src/holo/interpreter/nodes/calls/NewInstanceCallNode.java
+++ src/holo/interpreter/nodes/calls/NewInstanceCallNode.java
@@ -8,10 +8,10 @@ import holo.interpreter.nodes.Node;
 import holo.interpreter.nodes.helpers.args.NamedNode;
 import holo.interpreter.nodes.helpers.args.NamedValue;
 import holo.interpreter.nodes.var.VarPointAccessNode;
+import holo.interpreter.transcendental.TError;
 import holo.interpreter.values.Value;
 import holo.interpreter.values.interfaces.IInstanciable;
 import holo.lang.lexer.Sequence;
-import holo.transcendental.TError;
 
 public record NewInstanceCallNode(CallNode call, Sequence sequence) implements Node {
 
@@ -22,18 +22,17 @@ public record NewInstanceCallNode(CallNode call, Sequence sequence) implements N
 		if(call.access() instanceof VarPointAccessNode pointAccess) {
 			hostValue = pointAccess.access().interpret(parentContext, interpreter);
 			
-			accessValue = hostValue.pointGet(pointAccess.varName());
+			accessValue = hostValue.pointGet(pointAccess.varName(), sequence);
 			if(accessValue == null)
 				throw new TError(new CannotAccessError(hostValue.typeName(), call.access().sequence()));
 		} else accessValue = call.access().interpret(parentContext, interpreter);
 		
-		Value[] argsValues = new Value[call.args().length];
+		final Value[] argsValues = new Value[call.args().length];
 		for(int i = 0; i < call.args().length; i++)
 			argsValues[i] = call.args()[i].interpret(parentContext, interpreter);
 		
 		final NamedNode[] optionalArguments = call.optionalArguments();
-		
-		NamedValue[] optionalValues = new NamedValue[optionalArguments.length];
+		final NamedValue[] optionalValues = new NamedValue[optionalArguments.length];
 		for(int i = 0; i < optionalArguments.length; i++)
 			optionalValues[i] = new NamedValue(optionalArguments[i].name(), optionalArguments[i].node().interpret(parentContext, interpreter));
 		
