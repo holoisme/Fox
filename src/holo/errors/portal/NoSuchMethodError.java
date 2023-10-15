b535fb774a3781f839c94df45ad3a336a0e3b82a
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 20:54:39 +0200

    corrected text in case of null classes

--- src/holo/errors/portal/NoSuchMethodError.java
+++ src/holo/errors/portal/NoSuchMethodError.java
@@ -7,7 +7,7 @@ import holo.lang.lexer.Sequence;
 public class NoSuchMethodError extends RuntimeError {
 
 	public NoSuchMethodError(String name, Class<?>[] classes, String typeName, Sequence sequence) {
-		super("No such method", "Cannot find " + name + "("+ReflectionUtils.toString(classes)+") method in " + typeName, sequence);
+		super("No such method", "Cannot find " + name + (classes == null ? "" : "("+ReflectionUtils.toString(classes)+")") +" method in " + typeName, sequence);
 	}
 
 }
