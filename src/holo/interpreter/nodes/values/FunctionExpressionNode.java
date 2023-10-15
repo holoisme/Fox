467256e69c95947f5c2e6bfb012352909cad471f
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 20:52:07 +0200

    corrected toString representation

--- src/holo/interpreter/nodes/values/FunctionExpressionNode.java
+++ src/holo/interpreter/nodes/values/FunctionExpressionNode.java
@@ -6,6 +6,7 @@ import holo.interpreter.nodes.Node;
 import holo.interpreter.nodes.ReflectionUtils;
 import holo.interpreter.nodes.helpers.args.ObligatoryDefinitionArgument;
 import holo.interpreter.nodes.helpers.args.OptionalDefinitionArgument;
+import holo.interpreter.nodes.statements.ReturnNode;
 import holo.interpreter.values.Value;
 import holo.interpreter.values.functions.FunctionValue;
 import holo.lang.lexer.Sequence;
@@ -13,7 +14,7 @@ import holo.lang.lexer.Sequence;
 public record FunctionExpressionNode(ObligatoryDefinitionArgument[] regularArguments, OptionalDefinitionArgument[] optionalArguments, Node body, Sequence sequence) implements Node {
 	
 	public String toString() {
-		return "("+ReflectionUtils.toString(regularArguments)+") -> " + body;
+		return "("+ReflectionUtils.toString(regularArguments)+") -> " + (body instanceof ReturnNode n ? n.expression() : body);
 	}
 	
 	public Value interpret(Context parentContext, Interpreter interpreter) {
