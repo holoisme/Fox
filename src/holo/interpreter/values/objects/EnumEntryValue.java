2349f6189b7b80c0bf103944ce5c771be9cae871
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 19:00:10 +0200

    name and ordinal are now fields (temporary)

--- src/holo/interpreter/values/objects/EnumEntryValue.java
+++ src/holo/interpreter/values/objects/EnumEntryValue.java
@@ -1,24 +1,30 @@
 package holo.interpreter.values.objects;
 
-import holo.interpreter.contexts.Context;
 import holo.interpreter.values.Value;
-import holo.interpreter.values.prototypes.EnumEntryPrototype;
+import holo.interpreter.values.interfaces.IClass;
+import holo.interpreter.values.primitives.StringValue;
+import holo.interpreter.values.primitives.numbers.IntegerValue;
 
 public class EnumEntryValue extends ConstructedObjectValue {
 
-	private String name;
-	private int ordinal;
+	private final String name;
+	private final int ordinal;
 	
-	public EnumEntryValue(String enumClassName, Context enumClassDefiningContext, String name, int ordinal) {
-		super(enumClassName, enumClassDefiningContext);
+	public EnumEntryValue(IClass enumClass, String name, int ordinal) {
+		super(enumClass);
 		this.name = name;
 		this.ordinal = ordinal;
+		fields.put("name", new StringValue(name));
+		fields.put("ordinal", IntegerValue.get(ordinal));
 	}
 	
+//	@Override
+//	public Value pointGet(String key, Sequence sequence) {
+//		return fields.getOrDefault(key, EnumEntryPrototype.PROTOTYPE.get(key));
+//	}
+	
 	@Override
-	public Value pointGet(String key) {
-		return table.getOrDefault(key, EnumEntryPrototype.PROTOTYPE.get(key));
-	}
+	public Value typeOf() { return classValue.instanceTypeOf(); }
 	
 	public String name() { return name; }
 	public int ordinal() { return ordinal; }
