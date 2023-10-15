8a1827d6bd7c3426305228d331492b14bfaceb20
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 18:55:52 +0200

    added necessary arguments (interpreter and sequence) for error handling

--- src/holo/interpreter/values/interfaces/ICallHandler.java
+++ src/holo/interpreter/values/interfaces/ICallHandler.java
@@ -1,7 +1,10 @@
 package holo.interpreter.values.interfaces;
 
+import holo.interpreter.Interpreter;
+import holo.interpreter.nodes.helpers.args.NamedValue;
 import holo.interpreter.values.Value;
+import holo.lang.lexer.Sequence;
 
 public interface ICallHandler {
-	public Value callMethod(String name, Value... args);
+	public Value callMethod(String name, Value[] args, NamedValue[] optionalArguments, Interpreter interpreter, Sequence sequence);
 }
