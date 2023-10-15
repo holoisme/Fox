394e7260f2fccb9bd3138bcb1526c6825ac969dd
HoloInJava <yousseflaraki99@gmail.com>
2023-10-15 20:50:06 +0200

    added proper toString representation

--- src/holo/errors/trace/TraceEntry.java
+++ src/holo/errors/trace/TraceEntry.java
@@ -2,4 +2,10 @@ package holo.errors.trace;
 
 import holo.lang.lexer.Sequence;
 
-public record TraceEntry(String entry, Sequence sequence) {}
+public record TraceEntry(String entry, Sequence sequence) {
+	
+	public String toString() {
+		return entry + " at " + sequence;
+	}
+	
+}
